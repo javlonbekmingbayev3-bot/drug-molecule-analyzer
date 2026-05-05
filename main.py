30b64723f0c75ea4392a5db5dfb14279c9b736a2
Javlonbek Mingboev <javlonbekmingbayev3@gmail.com>
2026-05-05 18:13:26 +0500

    Enhance molecule input validation and processing
    Refactor molecule input handling and update function calls to use RDKit molecule object.

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from utils import molecular_weight, calculate_solubility, ro_5, display_formula, ADME
+from rdkit import Chem
 
 def main():
     print("===Welcome to Drug Molecule Analyzer program===")
-    molecule_structure = input("Enter a molecule in SMILES format: ")
+    while True:
+        molecule_structure = input("Enter a molecule in SMILES format: ")
+        mol = Chem.MolFromSmiles(molecule_structure)
+        if mol is not None:
+            break
+        else:
+            print("Invalid SMILES string. Try again.")
+            
     
-    print("Molecule weight: "+ str(molecular_weight(molecule_structure)))
-    print("Solubility: "+str(calculate_solubility(molecule_structure)))
-    print("Lipinski Status: "+str(ro_5(molecule_structure)))
+    print("\nMolecular weight: "+ str(molecular_weight(mol)))
+    print("Solubility: "+str(calculate_solubility(mol)))
+    print("Lipinski Status: "+str(ro_5(mol)))
     print("===Optional Menu===")
     while True:
         print("1. Save 2D Visualization \n2. Check ADME (tPSA & Rotatable bonds) \n3. Exit")
         decision = int(input(": "))
         match decision:
             case 1:
-                print(display_formula(molecule_structure))
+                display_formula(mol)
             case 2:
-                print(ADME(molecule_structure))
+                print(ADME(mol))
             case 3:
                 break
 
